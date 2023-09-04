2a163b99b611773e90c0b26170ecd1889b0790c2
ayanob <105100743+ayanob@users.noreply.github.com>
2023-09-04 19:41:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+
 import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
